e15b8c454a24e5465af89207f20adcc3fb72bc74
Douglas Carvalho <dscarvalho199@gmail.com>
2020-10-27 19:48:28 -0300

    property bind

--- src/app/views/list/list.component.ts
+++ src/app/views/list/list.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.scss'],
 })
 export class ListComponent implements OnInit {
+  selectedPkm = null;
+  nameFilter = '';
   pokemonList = [
     { name: 'Bulbasaur', number: 1 },
     { name: 'Charmander', number: 4 },
@@ -14,4 +16,14 @@ export class ListComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  get pkmSprite() {
+    const number = ('000' + this.selectedPkm.number).slice(-3);
+    return `//serebii.net/pokedex-xy/icon/${number}.png`;
+  }
+
+  selectPokemon(pkm) {
+    this.selectedPkm = pkm;
+    console.log(pkm);
+  }
 }
